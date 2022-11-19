4bbce09171b3019ff2141c7e350894b4457bd1ac
Ihor Odainyi <odayny@gmail.com>
2022-11-19 18:39:08 +0100

    refactored hilinksms.py

--- hilinksms.py
+++ hilinksms.py
@@ -1,89 +1,77 @@
-# A simple script to request additional 2GB for KPN unlimited plan using (Netherlands)
-# It should be scheduled somehow.
-# Logic:
-# 1. Check if there are unread SMS
-# 2. (Mark them as read)
-# 3. If there is one which says that 80% of package is used:
-# 4. Send sms to request another 2GB
-
-# Limitations:
-# - It developed for Huawei E3372 with software version 22.328.62.00.1217
-# - Password authorization is not implemented (Password can be disabled http://192.168.8.1/html/modifypassword.html)
-
-# Thanks to:
-# https://github.com/lmoodie/huawei-3G-SMS-API
-
+import os
 import traceback
+from datetime import datetime
+
 import requests
 import xmltodict
-from datetime import datetime
-import os
 
-SMS_LIST_TEMPLATE = '''<request>
-    <PageIndex>1</PageIndex>
-    <ReadCount>20</ReadCount>
-    <BoxType>1</BoxType>
-    <SortType>0</SortType>
-    <Ascending>0</Ascending>
-    <UnreadPreferred>1</UnreadPreferred>
-    </request>'''
-
-LOGGING = True
+USE_LOGGER = True
+DEVICE_IP = '192.168.8.1'
 
 
-def getHeaders(device_ip):
+def getHeaders():
+    """
+    Request headers for any further requests.
+    Sometimes headers became invalid for some reason so it's better to request them again for each request.
+    :return: headers dict
+    """
     token = None
-    sessionID = None
-    r = requests.get(url='http://' + device_ip + '/api/webserver/SesTokInfo')
-    d = xmltodict.parse(r.text, xml_attribs=True)
-    if 'response' in d and 'TokInfo' in d['response']:
-        token = d['response']['TokInfo']
-    d = xmltodict.parse(r.text, xml_attribs=True)
-    if 'response' in d and 'SesInfo' in d['response']:
-        sessionID = d['response']['SesInfo']
-    headers = {'__RequestVerificationToken': token, 'Cookie': sessionID}
-    return headers
+    session_id = None
+    resp = requests.get(url=f'http://{DEVICE_IP}/api/webserver/SesTokInfo')
+    resp_body = xmltodict.parse(resp.text, xml_attribs=True)
+    if 'response' in resp_body:
+        if 'TokInfo' in resp_body['response']:
+            token = resp_body['response']['TokInfo']
+        if 'SesInfo' in resp_body['response']:
+            session_id = resp_body['response']['SesInfo']
+        headers = {'__RequestVerificationToken': token, 'Cookie': session_id}
+        return headers
+    else:
+        raise Exception("Bad token request response")
 
 
-def getUnread(device_ip):
-    headers = getHeaders(device_ip)
-    r = requests.get(url = 'http://' + device_ip + '/api/monitoring/check-notifications', headers = headers)
-    d = xmltodict.parse(r.text, xml_attribs=True)
-    unread = int(d['response']['UnreadMessage'])
+def getUnreadCount():
+    headers = getHeaders()
+    response = requests.get(url=f'http://{DEVICE_IP}/api/monitoring/check-notifications', headers=headers)
+    response_body = xmltodict.parse(response.text, xml_attribs=True)
+    unread = int(response_body['response']['UnreadMessage'])
     return unread
 
 
-def getContent(data, numMessages):
-    messages = []
-    for i in range(numMessages):
-        message = data[i]
-        number = message['Phone']
-        content = message['Content']
-        date = message['Date']
-        messages.append('Message from ' + number + ' recieved ' + date + ' : ' + str(content))
+def getSMS():
+    sms_list_template = '''<request>
+        <PageIndex>1</PageIndex>
+        <ReadCount>20</ReadCount>
+        <BoxType>1</BoxType>
+        <SortType>0</SortType>
+        <Ascending>0</Ascending>
+        <UnreadPreferred>1</UnreadPreferred>
+        </request>'''
+    headers = getHeaders()
+    response = requests.post(url=f'http://{DEVICE_IP}/api/sms/sms-list', data=sms_list_template, headers=headers)
+    response_body = xmltodict.parse(response.text, xml_attribs=True)
+    messages_count = int(response_body['response']['Count'])
+    messages = response_body['response']['Messages']['Message']
+    if messages_count == 1:
+        temp = messages
+        messages = [temp]
     return messages
 
 
-def getSMS(device_ip):
-    headers = getHeaders(device_ip)
-    r = requests.post(url = 'http://' + device_ip + '/api/sms/sms-list', data = SMS_LIST_TEMPLATE, headers = headers)
-    d = xmltodict.parse(r.text, xml_attribs=True)
-    numMessages = int(d['response']['Count'])
-    messagesR = d['response']['Messages']['Message']
-    if numMessages == 1:
-        temp = messagesR
-        messagesR = [temp]
-    return messagesR
+def setRead(sms_id):
+    """
+    Mark SMS as read to skip it in the future.
+    :param sms_id: sms id (Index)
+    """
+    headers = getHeaders()
+    response = requests.post(url=f'http://{DEVICE_IP}/api/sms/set-read',
+                             data=f"<request><Index>{sms_id}</Index></request>", headers=headers)
+    response_body = xmltodict.parse(response.text, xml_attribs=True)
+    if response_body['response'] != "OK":
+        log(f"Cannot mark SMS {sms_id} as read. Hope it won't cause any issues in the future", "err")
 
 
-def setRead(device_ip, sms_id):
-    headers = getHeaders(device_ip)
-    r = requests.post(url = 'http://' + device_ip + '/api/sms/set-read', data = f"<request><Index>{sms_id}</Index></request>", headers = headers)
-    d = xmltodict.parse(r.text, xml_attribs=True)
-    return (d['response'])
-
-
-def sendSms(device_ip, number, content):
+def sendSms(number, content):
     template = f"""<request>
     <Index>-1</Index>
     <Phones><Phone>{number}</Phone></Phones>
@@ -94,36 +82,42 @@ def sendSms(device_ip, number, content):
     <Date>{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}</Date>
     </request>"""
 
-    headers = getHeaders(device_ip)
-    r = requests.post(url = 'http://' + device_ip + '/api/sms/send-sms',
-                      data = template, headers = headers)
-    d = xmltodict.parse(r.text, xml_attribs=True)
-    return (d['response'])
+    headers = getHeaders()
+    response = requests.post(url=f'http://{DEVICE_IP}/api/sms/send-sms',
+                             data=template, headers=headers)
+    response_body = xmltodict.parse(response.text, xml_attribs=True)
+    if response_body['response'] != "OK":
+        log(f"Cannot send SMS {content} to {number}.", "err")
+
 
 def log(message, tag):
-    if LOGGING:
+    if USE_LOGGER:
         os.system(f'logger -p {tag} -t hilinkssms "{message}"')
+    else:
+        print(f"{tag}: {message}")
 
 
-if __name__ == '__main__':
+def main():
     log("Checking for messages...", "notice")
     try:
-        device_ip = '192.168.8.1'
-        unread = getUnread(device_ip)
-        needToRenew = False
+        unread = getUnreadCount()
         if unread != 0:
-            log("Fetching sms list", "notice")
-            messages = getSMS(device_ip)
+            need_to_renew = False
+            log("Fetching SMS list", "notice")
+            messages = getSMS()
             for message in messages:
-                if message["Smstat"] == "0":
+                if message["Smstat"] == "0":  # if unread
                     if message["Content"].__contains__("NL2000 AAN") and message["Content"].__contains__("80%"):
-                        needToRenew = True
+                        need_to_renew = True
                         log("Found 80% notification", "notice")
-                    setRead(device_ip, message["Index"])
-        if needToRenew:
-            log("Requesting package", "notice")
-            sendSms(device_ip, "1266", "NL2000 AAN")
+                    setRead(message["Index"])
+            if need_to_renew:
+                log("Requesting package", "notice")
+                sendSms("1266", "NL2000 AAN")
     except Exception as e:
         log(traceback.format_exc(), "err")
     log("Finished", "notice")
 
+
+if __name__ == '__main__':
+    main()
